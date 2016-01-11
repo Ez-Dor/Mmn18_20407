ec660ace0f4e3ddc348143938b1e8121555d25bd
Ez-Dor <Dor348@gmail.com>
2016-01-11 13:31:01 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,4 @@
+//
+// Created by USER on 1/10/2016.
+//
+
---
+++ red_black_tree.h
@@ -0,0 +1,9 @@
+//
+// Created by USER on 1/10/2016.
+//
+
+#ifndef MMN18_20407_RED_BLACK_TREE_H
+#define MMN18_20407_RED_BLACK_TREE_H
+
+#endif //MMN18_20407_RED_BLACK_TREE_H
+ origin/master
